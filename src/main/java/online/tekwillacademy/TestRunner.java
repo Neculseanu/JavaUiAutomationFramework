44c0edf6fadf0fc4cc2864ad77bd55d15d8712fb
Neculseanu <neculseanuiulian@gmail.com>
2024-05-17 20:31:08 +0300

    Added the implementations for the driverManager

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,7 +1,29 @@
 package online.tekwillacademy;
 
+import online.tekwillacademy.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        String currentTabName = driver.getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver");
+Thread.sleep(5000);
+        driver.close();
+
+        driver.switchTo().window(currentTabName);
+
+        driver.get("https://tekwillacademy-opencart.online/");
+        driver.quit();
+
+
+        System.out.println("The driver is closed");
+
+
+
     }
 }
